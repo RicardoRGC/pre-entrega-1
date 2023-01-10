001c635ca61464607dd602037f1ac6ea91764dc9
RicardoRGC <gonzalezricardo.c90@gmail.com>
2023-01-10 18:29:58 -0300

    pc hp

--- src/Routes/ItemListContainer.jsx
+++ src/Routes/ItemListContainer.jsx
@@ -4,41 +4,45 @@ import ItemCard from "../components/ItemCard";
 import TostadaItems from "../components/TostadaItems";
 import { useEffect } from "react";
 import { useState } from "react";
-import { useParams} from "react-router-dom";
-
+import { useParams } from "react-router-dom";
 
 const ItemListContainer = () => {
   const params = useParams();
   const [datos, setDatos] = useState([]);
-  console.log(JSON.stringify(params) === '{}')
-  console.log(params)
-
-
+  // console.log(JSON.stringify(params) === "{}");
+  // console.log(datos);
 
   function handleFetch() {
-    fetch(`https://fakestoreapi.com/products${JSON.stringify(params) === '{}'?'':`/category/${params.id}`}` )
-    // fetch(`https://fakestoreapi.com/products` )
-    .then((response) => response.json())
-    .then((e) => setDatos(e))
-    .catch(e=>console.error('ERROR',e))
+    fetch(
+      !isNaN(params.id)
+        ? `https://fakestoreapi.com/products/${params.id}`
+        : `https://fakestoreapi.com/products${
+            JSON.stringify(params) === "{}" ? "" : `/category/${params.id}`
+          }`
+    )
+      // fetch(`https://fakestoreapi.com/products` )
+      .then((response) => response.json())
+      .then((e) => setDatos(e))
+      .catch((e) => console.error("ERROR", e));
   }
-  
-  
+
   useEffect(() => {
     handleFetch();
   }, [params]);
-// console.log(datos)
+  console.log(datos);
   return (
     <>
-    <Flex alignItems="center" justifyContent="center">
-
-    <TostadaItems/>
-    </Flex>
-      <Flex wrap='wrap'  alignItems="center" justifyContent="center" pt="10px">
-
-        {datos.map((greetingItem, index) => (
-          <ItemCard key={greetingItem.id} producto={greetingItem}></ItemCard>
-        ))}
+      <Flex alignItems="center" justifyContent="center">
+        <TostadaItems />
+      </Flex>
+      <Flex wrap="wrap" alignItems="center" justifyContent="center" pt="10px">
+        {datos.length == null ? (
+          <ItemCard key={datos.id} producto={datos}></ItemCard>
+        ) : (
+          datos.map((greetingItem, index) => (
+            <ItemCard key={greetingItem.id} producto={greetingItem}></ItemCard>
+          ))
+        )}
       </Flex>
     </>
   );
